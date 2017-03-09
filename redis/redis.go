3803d6c9358c4961cd383ed1a8cbca05ded57e42
thbkrkr <thb.krkr@gmail.com>
2017-03-09 02:24:25 +0100

    redis: create connections pool

--- redis/redis.go
+++ redis/redis.go
@@ -1,23 +1,59 @@
 package redis
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/garyburd/redigo/redis"
 )
 
-// NewConn creates a Redis connection
-func NewConn(redisURL string) redis.Conn {
-	conn, err := redis.Dial("tcp", redisURL)
+var maxConnections = 1000
+
+func init() {
+	maxConn := os.Getenv("REDIS_MAX_CONNECTIONS")
+	if maxConn != "" {
+		maxConnections, _ = strconv.Atoi(maxConn)
+	}
+}
+
+// NewConn creates a redis connection
+func NewConn(redisURL string, redisPwd string) redis.Conn {
+	conn, err := redis.Dial("tcp", redisURL, redis.DialPassword(redisPwd))
 	if err != nil {
-		logrus.WithError(err).Fatal("Fail to create redis connection")
+		logrus.WithError(err).WithField("url", redisURL).
+			Fatal("Fail to create redis connection")
 	}
 
 	_, err = conn.Do("PING")
 	if err != nil {
-		logrus.WithError(err).Fatal("Fail to ping redis")
+		logrus.WithError(err).WithField("url", redisURL).
+			Fatal("Fail to ping redis")
 	}
 
 	logrus.WithField("url", redisURL).Info("Redis connection ready")
 
 	return conn
+}
+
+// NewPool creates a redis connection pool
+func NewPool(redisURL string, redisPwd string) *redis.Pool {
+	pool := redis.NewPool(func() (redis.Conn, error) {
+		conn, err := redis.Dial("tcp", redisURL, redis.DialPassword(redisPwd))
+		if err != nil {
+			logrus.WithError(err).Fatal("Fail to create redis connection pool")
+			return nil, err
+		}
+
+		_, err = conn.Do("PING")
+		if err != nil {
+			logrus.WithError(err).Fatal("Fail to ping redis")
+			return nil, err
+		}
+
+		logrus.WithField("url", redisURL).Info("Redis connection ready")
+		return conn, nil
+	}, maxConnections)
+
+	return pool
 }
